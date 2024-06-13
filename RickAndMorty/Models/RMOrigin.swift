2378bc075707bb3dac29ff4d9f4366dde39387ed
Gleb <130483785+GP-iOS-23@users.noreply.github.com>
2024-06-13 11:10:12 +0500

    Add models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Глеб Поляков on 13.06.2024.
+//
+
+import Foundation
+
+struct RMOrigin: Codable{
+    let name: String
+    let url: String
+}
